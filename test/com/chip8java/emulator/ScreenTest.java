3e33c417627632588fb745bdb10869de42130f5c
Craig Thomas <craig.thomas@gmail.com>
2014-04-04 22:43:39 -0700

    Added tests for Screen routines.

---
+++ test/com/chip8java/emulator/ScreenTest.java
@@ -0,0 +1,118 @@
+package com.chip8java.emulator;
+
+import static org.junit.Assert.*;
+
+import java.awt.FontFormatException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ScreenTest {
+
+    private Screen mScreen;
+
+    @Before
+    public void setUp() throws FileNotFoundException, FontFormatException,
+            IOException {
+        mScreen = new Screen();
+    }
+
+    @After
+    public void tearDown() {
+        mScreen.dispose();
+    }
+
+    @Test
+    public void testDefaultConstructorSetsCorrectWidth() {
+        assertEquals(Screen.SCREEN_WIDTH, mScreen.getWidth());
+    }
+
+    @Test
+    public void testDefaultConstructorSetsCorrectHeight() {
+        assertEquals(Screen.SCREEN_HEIGHT, mScreen.getHeight());
+    }
+
+    @Test
+    public void testScaleFactorSetCorrectlyOnDefault() {
+        assertEquals(Screen.SCALE_FACTOR, mScreen.getScale());
+    }
+
+    @Test
+    public void testTitleSetCorrectlyOnDefault() {
+        assertEquals(Screen.DEFAULT_TITLE, mScreen.getTitle());
+    }
+
+    @Test
+    public void testScreenNoPixelsOnAtInit() {
+        for (int xCoord = 0; xCoord < mScreen.getWidth(); xCoord++) {
+            for (int yCoord = 0; yCoord < mScreen.getHeight(); yCoord++) {
+                assertFalse(mScreen.pixelOn(xCoord, yCoord));
+            }
+        }
+    }
+
+    @Test
+    public void testScreenTurningPixelsOnSetsPixelOn() {
+        for (int xCoord = 0; xCoord < mScreen.getWidth(); xCoord++) {
+            for (int yCoord = 0; yCoord < mScreen.getHeight(); yCoord++) {
+                mScreen.drawPixel(xCoord, yCoord, true);
+                assertTrue(mScreen.pixelOn(xCoord, yCoord));
+            }
+        }
+    }
+
+    @Test
+    public void testScreenTurningPixelsOffSetsPixelOff() {
+        for (int xCoord = 0; xCoord < mScreen.getWidth(); xCoord++) {
+            for (int yCoord = 0; yCoord < mScreen.getHeight(); yCoord++) {
+                mScreen.drawPixel(xCoord, yCoord, true);
+                mScreen.drawPixel(xCoord, yCoord, false);
+                assertFalse(mScreen.pixelOn(xCoord, yCoord));
+            }
+        }
+    }
+
+    @Test
+    public void testClearScreenSetsAllPixelsOff() {
+        for (int xCoord = 0; xCoord < mScreen.getWidth(); xCoord++) {
+            for (int yCoord = 0; yCoord < mScreen.getHeight(); yCoord++) {
+                mScreen.drawPixel(xCoord, yCoord, true);
+            }
+        }
+        mScreen.clearScreen();
+        for (int xCoord = 0; xCoord < mScreen.getWidth(); xCoord++) {
+            for (int yCoord = 0; yCoord < mScreen.getHeight(); yCoord++) {
+                assertFalse(mScreen.pixelOn(xCoord, yCoord));
+            }
+        }
+    }
+
+    @Test
+    public void testScaleFactorSetCorrectlyWithScaleConstructor()
+            throws FileNotFoundException, FontFormatException, IOException {
+        mScreen.dispose();
+        mScreen = new Screen(2);
+        assertEquals(2, mScreen.getScale());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeScaleFactorThrowsIllegalArgument()
+            throws FileNotFoundException, FontFormatException, IOException {
+        mScreen.dispose();
+        mScreen = new Screen(-1);
+    }
+
+    @Test
+    public void testWidthHeightScaleTitleSetCorrectlyConstructor()
+            throws FileNotFoundException, FontFormatException, IOException {
+        mScreen.dispose();
+        mScreen = new Screen(4, 5, 6, "test");
+        assertEquals(4, mScreen.getWidth());
+        assertEquals(5, mScreen.getHeight());
+        assertEquals(6, mScreen.getScale());
+        assertEquals("test", mScreen.getTitle());
+    }
+}
